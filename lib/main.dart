7772c92ccd82454c6a0855f46f4c10cfd3727bc0
PushpaKumara89 <kpushpakumarajd@gmail.com>
2023-07-06 11:48:58 +0530

    DevicePreview implementation( multiple mobile views )

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 import 'screens/home_page.dart';
 
-void main()=> runApp(const MyApp());
+void main()=> runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: "Test app",
-      home: MyHome(),
+      home: const MyHome(),
       /*routes: <String, WidgetBuilder>{
         '/second': (context)=> const SecondScreen(),
       },*/
